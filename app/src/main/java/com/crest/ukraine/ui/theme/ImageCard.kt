acae72adf14a37b1f146cc6cbb1b8bebcc840684
sergiikaplun <sikaplun@gmail.com>
2023-04-02 17:58:59 +0300

    adding card

---
+++ app/src/main/java/com/crest/ukraine/ui/theme/ImageCard.kt
@@ -0,0 +1,66 @@
+package com.crest.ukraine.ui.theme
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 20.dp,
+        border = BorderStroke(2.dp, Color.Blue)
+    ) {
+        Box(
+            modifier = Modifier.height(300.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Text(contentDescription, style = TextStyle(color = Color.White, fontSize = 24.sp))
+            }
+        }
+    }
+}
